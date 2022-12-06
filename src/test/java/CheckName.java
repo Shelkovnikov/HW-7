36356779cce39414e2117df115102664082e9036
Dmitry Shelkovnikov <misslook3@gmail.com>
2022-12-06 22:07:14 +0300

    Added @MethodSource Changed name HeaderNamesCheck-->headerNamesCheck

--- src/test/java/CheckName.java
+++ src/test/java/CheckName.java
@@ -2,7 +2,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CheckName {
-    public CheckName  HeaderNamesCheck(String Name) {
+    public CheckName  headerNamesCheck(String Name) {
         $(".tab-list__custom-wrap").shouldHave(text(Name));
         return this;
     }
--- src/test/java/RusspassTest.java
+++ src/test/java/RusspassTest.java
@@ -1,12 +1,18 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class RusspassTest {
+
+
     @BeforeEach
     public void openGeneralPage() {
         open("https://russpass.ru/");
@@ -18,7 +24,7 @@ public class RusspassTest {
     @ParameterizedTest(name = "Проверка содержимого верхнего меню главной страницы {0}")
     public void test02(String Name) {
         new CheckName()
-                .HeaderNamesCheck(Name);
+                .headerNamesCheck(Name);
     }
 
 
@@ -26,7 +32,27 @@ public class RusspassTest {
     @ValueSource(strings = {"Отели", "Музеи", "Экскурсии", "Авиабилеты", "Ж/Д билеты", "Направления"})
     public void test01(String Name) {
         new CheckName()
-                .HeaderNamesCheck(Name);
+                .headerNamesCheck(Name);
+
+    }
+
+    public static Stream<Arguments> test03() {
+        return Stream.of(
+                Arguments.of("Отели"),
+                Arguments.of("Музеи"),
+                Arguments.of("Экскурсии"),
+                Arguments.of("Авиабилеты"),
+                Arguments.of("Ж/Д билеты"),
+                Arguments.of("Направления")
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Проверка содержимого верхнего меню главной страницы {0}")
+
+    public void test03(String Name) {
+        new CheckName()
+                .headerNamesCheck(Name);
 
     }
 }
